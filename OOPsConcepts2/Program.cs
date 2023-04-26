aaa39fab66035bf7ef63ee7d35a31ecfa207d74e
Sushil <skshinde2028@gmail.com>
2023-04-26 15:53:29 +0530

    [Sushil] Implementing encapsulation

--- OOPsConcepts2/Program.cs
+++ OOPsConcepts2/Program.cs
@@ -11,14 +11,18 @@ namespace OOPsConcepts2
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the oops concepts");
-            PartTimeEmp partTimeEmp = new PartTimeEmp();
-            partTimeEmp.empName = "Sushil";
-            FullTimeEmp fullTimeEmp = new FullTimeEmp();
-            fullTimeEmp.empName = "Sachin";
+            //Encapsulation
+            Person person = new Person();
+            //person.setName("sushil");
+            //person.getName();
+            //person.setAge(21);
+            //person.getAge();
 
-            fullTimeEmp.EmployeeDetails();
- 
-            Console.WriteLine(fullTimeEmp.empName);
+            //private variables are accessed by method or properties
+            person.age = 23;
+            person.getName = "Sushil";
+            Console.WriteLine($"Person's age is {person.getAge}");
+            Console.WriteLine($"Person's name is {person.getName}");
         }
     }
 }
